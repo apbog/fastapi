da7443c8bd3159b70b15a6c83fea3ed9923300b0
apbog <apbogolyubov@gmail.com>
2021-04-16 00:33:56 +0300

    Upd code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from typing import Optional
+from typing import Optional, List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
 
 task_api = FastAPI()
@@ -13,20 +13,44 @@ class Task(BaseModel):
 
 
 @task_api.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 
-@task_api.get("/tasks/{task_id}")
-def read_task(task_id: int, q: Optional[str] = None):
+@task_api.get("/tasks/get_by_id/{task_id}")
+async def get_task_by_id(task_id: int, q: Optional[str] = None):
     return {"task_id": task_id, "q": q}
 
 
-@task_api.put("/tasks/{task_id}")
-def update_task(task_id: int, task: Task):
+@task_api.get("/tasks/get_by_title/")
+async def get_task_by_title(
+        task_title: str = Query(
+            ...,
+            alias="task-title",
+            title="Task title",
+            description="Task title to search in the database that have a good match",
+            min_length=5,
+            max_length=50
+        )
+):
+    return {"task_title": task_title}
+
+
+@task_api.get("/tasks/get_list/")
+async def get_task_by_title(task_titles: List[str] = Query(...)):
+    return {"task_titles": task_titles}
+
+
+@task_api.put("/tasks/update/{task_id}")
+async def update_task(task_id: int, task: Task):
     return {"task_name": task.title, "task_id": task_id}
 
 
 @task_api.post("/tasks/add")
-def read_task(task_id: int):
+async def add_task(task_id: int):
     return task_id
+
+
+@task_api.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
